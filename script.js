519099d88a62cb3b91e987d0a121bb5de4de460e
Sholto <madein.mania@gmail.com>
2021-02-17 11:52:22 +0000

    Gameboard > Game module

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
-//Gameboard object
-const GameBoard = () => {
+//Gameboard object - module
+const Game = (() => {
 
     const x = new Array(3);
 
     const board = () => {
-        
+
         for (let i = 0; i < x.length; i++) {
             x[i] = new Array(3);
-            
+
         };
         console.log(x);
     };
 
-    return {board};
-}
+    return {
+
+        board
+
+    };
+
+})();
 
 //Player Object
 const Player = (name) => {
 
     const sayName = () => console.log(`my name is ${name}`)
-    return {sayName};    
+    return { sayName };
 }
-
