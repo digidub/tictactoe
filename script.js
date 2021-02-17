b10025c14c8324c5970105e2fed28d2cee9e270e
Sholto <madein.mania@gmail.com>
2021-02-17 14:30:36 +0000

    working on game cell generation

--- script.js
+++ script.js
@@ -24,5 +24,33 @@ const Game = (() => {
 const Player = (name) => {
 
     const sayName = () => console.log(`my name is ${name}`)
+
     return { sayName };
 }
+
+
+const populateBoard = (() => {
+
+    const testy = () => {
+        console.log(this)
+    }
+
+    const _gameBoard = document.querySelector('.board')
+    let _cell = ""; 
+       
+
+    const populate = () => {
+        for (let i = 0; i < 9; i++) {
+            _cell = document.createElement("div")
+            _cell.setAttribute('class', 'cell'); 
+            _gameBoard.appendChild(_cell);
+        }
+    }
+
+    return {
+        populate,
+        testy
+    }
+
+})();
+
