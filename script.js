72847f78307e114ceaeffd9a601a2c719f30c9ea
Sholto <madein.mania@gmail.com>
2021-02-18 16:32:15 +0000

    small readability tweaks

--- script.js
+++ script.js
@@ -2,17 +2,28 @@
 const MakeGame = (() => {
 
     const x = new Array(3);
+
     const board = () => {
+
         for (let i = 0; i < x.length; i++) {
+
             x[i] = new Array(3);
+
             for (let j = 0; j < x[i].length; j++) {
+
                 x[i][j] = `${i}${j}`;
+
             };
+
         };
+
         return x;
     };
+
     return {
+
         board
+
     };
 })();
 
@@ -20,19 +31,20 @@ const MakeGame = (() => {
 const GameBoard = (() => {
 
     const board = MakeGame.board();
-    console.log(board);
     const _gameBoard = document.querySelector('.board')
     let _cell;
 
     const populate = () => {
 
         for (let x = 0; x < board.length; x++) {
+
             for (let y = 0; y < board[x].length; y++) {
                 _cell = document.createElement("div");
                 _cell.setAttribute('class', 'cell');
                 _cell.setAttribute('id', `${board[x][y]}`)
                 _cell.innerHTML = board[x][y];
                 _gameBoard.appendChild(_cell);
+
             }
         }
     }
@@ -46,46 +58,55 @@ const GameBoard = (() => {
 const Player = (playerName, piece) => {
 
     const getName = () => playerName;
-    //let getPiece = () => piece; IGNORE THIS FOR NOW
+    const getPiece = () => piece;
 
     return {
+
         getName,
-        //getPiece
+        getPiece
+
     };
 }
 
 const gameState = (() => {
 
+    GameBoard.populate();
+
     let turn;
 
     let player1 = Player("Alex");
     let player2 = Player("Bob");
+    let p1name = player1.getName()
+    let p2name = player2.getName()
               
     const whoStarts = () => {
 
         let coinFlip = Math.round(Math.random());
-        if (coinFlip === 0) return turn = player1.getName()
-        else return turn = player2.getName()
+        if (coinFlip === 0) return turn = p1name
+        else return turn = p2name;
+
     };    
 
-    let nextTurn = (turn) => {
-        if (turn == player1.getName()) {
-            turn = player2.getName()
+    let nextTurn = () => {
+
+        if (turn == p1name) {
+            turn = p2name            
         }
-        else if (turn == player2.getName()) {
-            turn = player1.getName()
+        else if (turn == p2name) {
+            turn = p1name            
         }
+
     }
 
     return {
-        whoStarts,
-        //whoseTurn,
+
+        whoStarts,        
         nextTurn,
         Player
+
     }
 
 })();
 
 
 
-GameBoard.populate();
