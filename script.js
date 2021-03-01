b631598f04d2dbd73d8dc0ed44c69175270fb8d8
Sholto <madein.mania@gmail.com>
2021-03-01 10:25:14 +0000

    working minimax algorithm

--- script.js
+++ script.js
@@ -96,7 +96,6 @@ const Player = (playerName) => {
             for (j = 0; j < MakeGame.board[i].length; j++) {
                 if ((MakeGame.board[i][j] != "X") && (MakeGame.board[i][j] != "O")) {
                     freeSpaceArray.push(MakeGame.board[i][j]);
-                    console.log(freeSpaceArray);
                 }
             }
         }
@@ -363,4 +362,151 @@ const gameState = (() => {
     }
 
 })();
+
+
+
+//testing AI:
+
+let origBoard = [
+    [00,01,02],
+    [10, "O", 12],
+    [20, 21, 22],
+]
+
+let huPlayer = "O";
+let aiPlayer = "X";
+
+//creates 2D array of available spaces to play
+function emptySpace(arr) {
+    const freeSpaceArray = [];
+    for (i = 0; i < arr.length; i++) {
+        for (j = 0; j < arr[i].length; j++) {
+            if ((arr[i][j] != "X") && (arr[i][j] != "O")) {
+                freeSpaceArray.push(arr[i][j]);
+            }
+        }
+    }
+    return freeSpaceArray;
+}
+
+
+//checks 3D array for a winner
+function checkWinner(board, player) {
+
+    const diagUpEqual = (player, board) => ((board[0][0] === player) && (board[0][0] === board[1][1]) && (board[0][0] === board[2][2]));
+
+    const diagDownEqual = (player, board) => ((board[0][2] === player) && (board[0][2] === board[1][1]) && (board[0][2] === board[2][0]));
+
+    const rowEqual = function (player, board) {
+        for (i = 0; i < board.length; i++) {
+            if (xCheck(board[i], player)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    const xCheck = (board, player) => board.every(val => val === player);
+
+    const columnEqual = function (player, board) {
+        for (i = 0; i < board[0].length; i++) {
+            if ((board[0][i] === player) && (board[0][i] === board[1][i]) && (board[0][i] === board[2][i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    if ((diagUpEqual(player, board)) || (diagDownEqual(player, board)) || (rowEqual(player, board)) || (columnEqual(player, board))) {
+        return true
+    }
+    else return false;
+
+}
+
+
+
+let bestSpot = minimax(origBoard, aiPlayer)
+console.log("index:" + bestSpot.index)
+console.log("function calls: " + functionCalls)
+
+
+
+//minimax function
+function minimax(newBoard, player, depth=0) {
+
+    let availSpots = emptySpace(origBoard);    
+
+    if (checkWinner(newBoard, huPlayer)) {
+        return { score: depth - 100 };
+    } else if (checkWinner(newBoard, aiPlayer)) {
+        return { score: 100 - depth };
+    } else if (availSpots.length == 0) {
+        return { score: 0 };
+    }    
+
+    let moves = [];
+
+    for (let i = 0; i < availSpots.length; i++) {
+
+        let move = {};
+
+        //check co-ordinates of board 
+        if (availSpots[i] < 4) {
+            move.index = newBoard[0][availSpots[i]]
+            newBoard[0][availSpots[i]] = player
+        } else {
+            splitCoOrds = availSpots[i].toString().split("");
+            let xCoOrd = splitCoOrds[0]
+            let yCoOrd = splitCoOrds[1]
+            move.index = newBoard[xCoOrd][yCoOrd]
+            newBoard[xCoOrd][yCoOrd] = player
+        };
+
+        //recursive function call
+        if (player == aiPlayer) {            
+            result = minimax(newBoard, huPlayer, depth+1);
+            move.score = result.score;
+        } else {            
+            result = minimax(newBoard, aiPlayer, depth+1);
+            move.score = result.score;
+        };
+
+        //test to reset the board position to empty depending on coordinates
+        if (availSpots[i] < 4) {
+            newBoard[0][availSpots[i]] = move.index
+        }
+        else {
+            let splitCoOrds2 = availSpots[i].toString().split("");
+            let xCoOrd = splitCoOrds2[0];
+            let yCoOrd = splitCoOrds2[1];
+            newBoard[xCoOrd][yCoOrd] = move.index
+        };
+        
+        moves.push(move);
+
+
+    }
+    let bestMove;
+    if (player === aiPlayer) {
+        let bestScore = -10000;
+        for (let i = 0; i < moves.length; i++) {
+            if (moves[i].score > bestScore) {
+                bestScore = moves[i].score;
+                bestMove = i;
+            }
+        }
+    } else {
+
+        let bestScore = 10000;
+        for (let i = 0; i < moves.length; i++) {
+            if (moves[i].score < bestScore) {
+                bestScore = moves[i].score;
+                bestMove = i;
+            }
+        }
+    }    
+    return moves[bestMove];
+
+}
 
